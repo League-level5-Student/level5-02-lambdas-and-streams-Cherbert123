6cfeeac7a81779ec0ae0c6c2a3a0fa89bed984e5
League Student <league@adminisorsiMac3.attlocal.net>
2019-02-03 12:54:51 -0800

    Buttons and Methods

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -12,14 +12,48 @@ public class LambdaMethods {
 			}
 		}, "repeat");
 		
-		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		//2. Call the printCustomMessage method using a lambda so that the String prints backwards.
+		printCustomMessage((s)->{
+			String print = "";
+			for(int i = 0; i < s.length(); i++) {
+				print = print + s.charAt(s.length() - i - 1);
+			}
+			System.out.println(print);
+		}, "backwards");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			String print = "";
+			boolean up = true;
+			for(int i = 0; i < s.length(); i++) {
+				if (up) {
+				print = print + s.substring(i, i+1).toUpperCase();
+				up = false;
+				} else {
+				print = print + s.substring(i, i+1).toLowerCase();
+				up = true;
+				}
+			}
+			System.out.println(print);
+		}, "UpperLower");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			String print = "";
+			for(int i = 0; i < s.length(); i++) {
+				print = print + s.substring(i, i+1) + ".";
+			}
+			System.out.println(print);
+		}, "Periods");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)->{
+			String print = "";
+			for(int i = 0; i < s.length(); i++) {
+				if(!s.substring(i, i+1).equals("e") && !s.substring(i, i+1).equals("a") && !s.substring(i, i+1).equals("i") && !s.substring(i, i+1).equals("o") && !s.substring(i, i+1).equals("u")) {
+				print = print + s.substring(i, i+1);
+			
+				}
+			}
+			System.out.println(print);
+		}, "Voweless");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
